12a9be49d191493a75d198ea3f727a50fa37f967
_gorilla_ <t.workout.r@gmail.com>
2022-11-04 22:53:14 +0900

    別のカラムにタスクを追加する機能を追加 完成

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,8 +10,45 @@ const Main = () => {
         console.log(result);
         const { source, destination } = result;
 
-        // 同じカラム内でのタスクの入れ替え
-        
+        if (source.droppableId !== destination.droppableId) {
+            // 別のカラムにタスクが移動したとき
+            const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+            const destinationColIndex = data.findIndex((e) => e.id === destination.droppableId);
+            
+            const sourceCol = data[sourceColIndex];
+            const destinationCol = data[destinationColIndex];
+
+            const sourceTask = [...sourceCol.tasks];
+            const destinationTask = [...destinationCol.tasks];
+
+            // 動かし始めたタスクを削除
+            const [removed] = sourceTask.splice(source.index, 1);
+
+            // 動かした後のカラムにタスクを追加
+            destinationTask.splice(destination.index, 0, removed);
+
+            data[sourceColIndex].tasks = sourceTask;
+            data[destinationColIndex].tasks = destinationTask;
+
+            setData(data);
+        } else {
+            // 同じカラム内でのタスクの入れ替え
+            const sourceColIndex = data.findIndex((e) => e.id === source.droppableId);
+            console.log(sourceColIndex);
+            const sourceCol = data[sourceColIndex];
+            console.log(sourceCol);
+
+            const sourceTask = [...sourceCol.tasks];
+
+            // タスクを削除
+            const [removed] = sourceTask.splice(source.index, 1);
+
+            // タスクを追加
+            sourceTask.splice(destination.index, 0, removed);
+
+            data[sourceColIndex].tasks = sourceTask;
+            setData(data);
+        }
     };
 
     return (
